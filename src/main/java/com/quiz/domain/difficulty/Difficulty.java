f4fff98f4a16ba8810e355c564ab8e0306653482
Andras Torok <torokandras1996@gmail.com>
2020-03-14 13:53:31 +0100

    Created more enums.
    Category, Difficulty and Type.

--- src/main/java/com/quiz/domain/difficulty/Difficulty.java
+++ src/main/java/com/quiz/domain/difficulty/Difficulty.java
@@ -1,4 +1,19 @@
 package com.quiz.domain.difficulty;
 
+import lombok.Getter;
+
+@Getter
 public enum Difficulty {
+
+    EASY("easy", 1),
+    MEDIUM("medium", 2),
+    HARD("hard", 3);
+
+    private final String difficulty;
+    private final int point;
+
+    Difficulty(String difficulty, int point) {
+        this.difficulty = difficulty;
+        this.point = point;
+    }
 }
--- src/main/java/com/quiz/domain/type/Type.java
+++ src/main/java/com/quiz/domain/type/Type.java
@@ -1,4 +1,16 @@
 package com.quiz.domain.type;
 
+import lombok.Getter;
+
+@Getter
 public enum Type {
+
+    TRUE_FALSE("boolean"),
+    MULTIPLE_CHOICE("multiple");
+
+    private final String type;
+
+    Type(String type) {
+        this.type = type;
+    }
 }
