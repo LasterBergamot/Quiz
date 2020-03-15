51ef7cad8999522e040a45624f35518e0cbe77be
Andras Torok <torokandras1996@gmail.com>
2020-03-15 09:49:43 +0100

    The home page now shows up.

--- src/main/java/com/quiz/controller/QuizController.java
+++ src/main/java/com/quiz/controller/QuizController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.quiz.domain.trivia.TriviaDTOList;
 import com.quiz.service.IQuizService;
@@ -11,7 +12,9 @@ import com.quiz.service.IQuizService;
 @Controller
 public class QuizController {
 
-    private final String HOME = "/";
+    private final String GET_MAPPING_HOME = "/";
+
+    private final String VIEW_HOME = "home";
 
     private IQuizService quizService;
     private RestTemplate restTemplate;
@@ -22,12 +25,11 @@ public class QuizController {
         this.restTemplate = restTemplate;
     }
 
-    @GetMapping(HOME)
-    public void printTrivia() {
+    @GetMapping(GET_MAPPING_HOME)
+    public ModelAndView showHomePage() {
        TriviaDTOList triviaDTOList = restTemplate.getForObject("https://opentdb.com/api.php?amount=10", TriviaDTOList.class);
        quizService.printAllTrivia(triviaDTOList.getTriviaDTOList());
 
-//        ResponseEntity<TriviaDTO[]> responseEntity = restTemplate.getForEntity("https://opentdb.com/api.php?amount=10", TriviaDTO[].class);
-//        System.out.println(Arrays.toString(responseEntity.getBody()));
+       return new ModelAndView(VIEW_HOME);
     }
 }
